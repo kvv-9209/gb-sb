da701fc849a4bde9a57a9aadf72b15d0e346718f
kvv-9209 <kvv-9209@mail.ru>
2022-04-06 20:30:14 +0300

    master

---
+++ src/main/java/ru/gb/gbsb/GbSbApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gbsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbSbApplication.class, args);
+    }
+
+}
